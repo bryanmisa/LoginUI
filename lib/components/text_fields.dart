ebdae2ca6ca8ed0e6cd42e9d70b09f436169cf66
Bryan Robertson Misa <bryan.misa@gmail.com>
2023-12-17 21:58:53 +0300

    Project Initialize

---
+++ lib/components/text_fields.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TextFieldWidgets extends StatelessWidget {
+  const TextFieldWidgets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
